39121466b2a6da7c4f435e41de62456438653473
Iliass Raihani <i.raihani@outlook.com>
2020-02-25 02:32:41 +0100

    add browser sync cmd gulp

--- Brief Projet Niv05 - Front-end web application/fichier_starter/gulpfile.js
+++ Brief Projet Niv05 - Front-end web application/fichier_starter/gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
 
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+// Live Server
+gulp.task('browserSync', function() {
+    browserSync.init({
+      server: {
+        baseDir: 'app'
+      },
+    })
+  })
+
 // Compilation SASS 
 gulp.task('sass', function() {
     return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
