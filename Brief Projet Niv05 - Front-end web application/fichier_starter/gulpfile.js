8dacf5ad371556e9fa98d7ba8fa8fc0df04a68c5
Iliass Raihani <i.raihani@outlook.com>
2020-02-25 11:40:08 +0100

    add minify img

--- Brief Projet Niv05 - Front-end web application/fichier_starter/gulpfile.js
+++ Brief Projet Niv05 - Front-end web application/fichier_starter/gulpfile.js
@@ -6,6 +6,8 @@ var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
 
 // Live Server
 gulp.task('browserSync', function() {
@@ -39,4 +41,13 @@ gulp.task('useref', function(){
       // Minifies only if it's a CSS file
       .pipe(gulpIf('*.css', cssnano()))
       .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/images'))
 });
