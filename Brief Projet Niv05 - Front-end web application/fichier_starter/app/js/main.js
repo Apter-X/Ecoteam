a190d4e614806a99f44fb42dca890e70364e0207
Iliass Raihani <i.raihani@outlook.com>
2020-03-01 12:52:35 +0100

    animate open sidebar

--- Brief Projet Niv05 - Front-end web application/fichier_starter/app/js/main.js
+++ Brief Projet Niv05 - Front-end web application/fichier_starter/app/js/main.js
@@ -1,4 +1,19 @@
-function open() {
-    document.getElementById("main").style.marginLeft = "25%";
-    document.getElementById("mySidebar").style.width = "25%";
+function open_close() {
+    document.getElementById("mySidebar").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+    document.getElementById("openNav").style.width = "200px";
+    // document.getElementById("mySidebar").style.display = "block";
+    // document.getElementById("openNav").style.display = 'none';
   }
+  function _close() {
+    document.getElementById("main").style.marginLeft = "0";
+//     document.getElementById("mySidebar").style.display = "none";
+    // document.getElementById("openNav").style.display = "inline-block";
+  }
+
+//   var x = document.getElementById("mySidebar");
+//   if (x.style.width === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
