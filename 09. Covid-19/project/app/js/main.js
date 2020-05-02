e0c7602064b563bbfb52fa17cabf8cc35013e820
Iliass Raihani <i.raihani@outlook.com>
2020-05-02 17:58:49 +0000

    Update main.js

--- 09. Covid-19/project/app/js/main.js
+++ 09. Covid-19/project/app/js/main.js
@@ -1,24 +1,20 @@
-class Slider{
-  constructor(){
-    this.progress = progress;
-  }
+var progress;
 
-  init(){
-    this.progress = 1;
-  }
+function init(){
+  this.progress = 1;
+}
 
-  moveSlide( n ){
-    showSlide( progress += n );
-  }
+function moveSlide( n ){
+  showSlide( progress += n );
+}
 
-  currentSlide( n ){
-    showSlide( progress = n );
-  }
+function currentSlide( n ){
+  showSlide( progress = n );
+}
 
-  showSlide(){
-    var i;
-    var slides = document.getElementsByClassName("mySlider");
-    var bar = document.getElementsByClassName("bar");
+function showSlide(){
+  var i;
+  var slides = document.getElementsByClassName("mySlider");
+  var bar = document.getElementsByClassName("bar");
 
-  }
 }
