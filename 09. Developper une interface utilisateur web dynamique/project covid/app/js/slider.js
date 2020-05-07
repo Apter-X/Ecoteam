1752506589cf47d796fb3f3b1dd68d370ddfd70a
Iliass Raihani <i.raihani@outlook.com>
2020-05-07 12:34:59 +0000

    add questions

--- 09. Developper une interface utilisateur web dynamique/project covid/app/js/slider.js
+++ 09. Developper une interface utilisateur web dynamique/project covid/app/js/slider.js
@@ -3,7 +3,8 @@ var bar = document.getElementsByClassName("progress-bar");
 var labelBar = document.getElementsByClassName("progress-bar__label");
 var btnRight = document.getElementById('btn_right');
 var btnLeft = document.getElementById('btn_left');
-var progress;
+var question = document.getElementById('question');
+var progress = 0;
 
 // window.addEventListener("beforeunload", function (e) {
 //   var confirmationMessage = "\o/";
@@ -12,42 +13,27 @@ var progress;
 //   return confirmationMessage;                            //Webkit, Safari, Chrome
 // });
 
-init();
 
-function init(){
-  progress = 1;
-  showSlide(progress);
-}
+showSlide(progress);
+
 
 function moveSlides( n ){
   showSlide( progress += n );
 }
 
-function currentSlide( n ){
-  showSlide( progress = n );
-}
-
-
-
 function showSlide(n){
-  var i;
 
-  // if ( n = slides.length ){
-  //   btnRight.disabled = true;
-  // } 
-
-  // if ( n = 1 ){
-  //   btnLeft.disabled = true;
-  //   console.log('Button left disabled!');
-  // }else{
-  //   btnLeft.disabled = false;
-  //   console.log('Button left enabled!');
-  // }
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  if (n === 0){
+    btnLeft.disabled = true;
+  }else{
+    btnLeft.disabled = false;
   }
 
-  slides[progress-1].style.display = "block";
-  console.log(progress);
+  if ( n === questions.length - 1){
+    btnRight.disabled = true;
+  }else{
+    btnRight.disabled = false;
+  }
+
+  question.innerHTML = questions[n];
 }
